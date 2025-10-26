7a0f2e793172f89d796f589bd0f3cea59bb12b41
Alchemmist <anton.ingrish@gmail.com>
2025-10-26 18:34:44 +0300

    setup custom styles for remark

--- site/assets/remark42-custom.js
+++ site/assets/remark42-custom.js
@@ -1,3 +1,16 @@
+var MutationObserver =
+  window.MutationObserver ||
+  window.WebKitMutationObserver ||
+  window.MozMutationObserver;
+
+var observer = new MutationObserver(function (mutations) {
+  if (document.querySelector("#remark42 iframe")) {
+    var iframe = document.querySelector("#remark42 iframe");
+
+    iframe.addEventListener("load", function () {
+      var style = document.createElement("style");
+      style.id = "custom-remark-styles";
+      style.textContent = `
 @font-face {
   font-family: "CourierCyr";
   src: url("https://cdn.jsdelivr.net/gh/alchemmist/personal-site@main/site/static/assets/fonts/couriercyrps.woff2")
@@ -34,6 +47,15 @@
   font-display: swap;
 }
 
-#remark42 {
+body {
   font-family: nerdfontssymbols nerd font, "CourierCyr", serif !important;
 }
+      `;
+      iframe.contentDocument.head.appendChild(style);
+      observer.disconnect();
+    });
+  }
+});
+
+var targetNode = document.getElementById("remark42");
+if (targetNode) observer.observe(targetNode, { childList: true });
